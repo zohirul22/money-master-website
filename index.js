42ef1d9085c0e5ed7a1e3b7d9c679f3976c7422b
zohirulislam1 <codershahin84@gmail.com>
2022-02-17 17:06:06 +0600

    fourtly commit

--- index.js
+++ index.js
@@ -1,6 +1,64 @@
 document.getElementById('calculet-btn').addEventListener('click',function(){
 
-   const foodExpenses = document.getElementById('food-expen').innerText;
+const foodExpenses = document.getElementById('food-expen');
+const foodExpensesText = foodExpenses.value;
+
+
+const rentExpenses = document.getElementById('rent-expen');
+const rentExpensesText = rentExpenses.value;
+
+
+const clothesExpenses = document.getElementById('clothes-expen');
+const clothesExpensesText = clothesExpenses.value;
+
+
+
+const totalExpenses = document.getElementById('total-expen');
+const totalExpensesText = totalExpenses.value;
+
+totalExpenses.value = parseFloat(foodExpensesText) + parseFloat(rentExpensesText) + parseFloat(clothesExpensesText);
+
+
+ const incomeMoney = document.getElementById('salary');
+const incomeMoneytext = incomeMoney.value;
+
+const totalIncome = parseFloat(incomeMoneytext);
+
+const remaingBalance = document.getElementById('total-balance');
+const remaingBalanceText = remaingBalance.value;
+const remaingBalanceAmount = parseFloat(remaingBalanceText);
+
+remaingBalance.value = totalIncome - totalExpenses.value;
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* document.getElementById('calculet-btn').addEventListener('click',function(){
+
+   
+   
+ const foodExpenses = document.getElementById('food-expen').innerText;
    const rentExpenses = document.getElementById('rent-expen').innerText;
    const clothesExpenses = document.getElementById('clothes-expen').innerText;
 
@@ -18,7 +76,7 @@ const totalBalanceText = document.getElementById('total-balance');
 
 totalBalanceText.innerText = totalSalary - totalExpenses.innerText;
 
+  
+  
 
-   
-
-})
+}) */
