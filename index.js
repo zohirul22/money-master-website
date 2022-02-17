6d5fa0f797ad4c37f45ea6d359771533e7934e48
zohirulislam1 <codershahin84@gmail.com>
2022-02-17 10:57:24 +0600

    third time commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('shahhin')
+document.getElementById('calculet-btn').addEventListener('click',function(){
+
+   const foodExpenses = document.getElementById('food-expen').innerText;
+   const rentExpenses = document.getElementById('rent-expen').innerText;
+   const clothesExpenses = document.getElementById('clothes-expen').innerText;
+
+ const expensesValue = parseFloat(foodExpenses) + parseFloat(rentExpenses) + parseFloat(clothesExpenses);
+
+ const totalExpenses = document.getElementById('total-expen');
+
+totalExpenses.innerText = expensesValue;
+
+const totalSalarytext = document.getElementById('salary').innerText;
+
+const totalSalary = parseFloat(totalSalarytext);
+
+const totalBalanceText = document.getElementById('total-balance');
+
+totalBalanceText.innerText = totalSalary - totalExpenses.innerText;
+
+
+   
+
+})
